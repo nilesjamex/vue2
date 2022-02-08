1bca149a3997b54b801be8bc7056cfdc6c3c91a9
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-08 22:23:57 +0100

    added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <template>
+<div>
+  <!-- As a heading -->
+  <b-navbar variant="faded" type="light">
+    <b-navbar-brand tag="h1" class="mb-0">Breaking Bad</b-navbar-brand>
+  </b-navbar>
+</div>
   <router-view/>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-createApp(App).use(router).use(BootstrapVue).mount('#app')
+// Vue.use(BootstrapVue)
+// Vue.use(BootstrapVueIcons)
+createApp(App).use(router).mount('#app')

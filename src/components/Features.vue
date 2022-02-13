10df377bfb414f7eae5e9eaca5724de2a4abbd96
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-13 23:37:14 +0100

    completed and imported features component

---
+++ src/components/Features.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="featureMain">
+      <div class="featurelist" v-for="feature in features" :key="feature.title">
+          <img src="../assets/green.png" alt="">
+          <h2> {{ feature.title }} </h2>
+          <p> {{ feature.content }} </p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            features: [
+                {
+                title: 'Market Place', 
+                content: 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings.'
+            },
+            {
+                title: 'Rare Collection', 
+                content: 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings.'
+            },
+            {
+                title: 'Ethereum Network', 
+                content: 'A wonderful serenity has taken possession of my entire soul, like these sweet mornings.'
+            }
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.featureMain {
+    display: flex;
+    grid-gap: 5rem;
+    justify-content: space-around;
+    text-align: center;
+    padding: 3% 9%;
+}
+.featurelist {
+    width: 310px;
+    height: 206px;
+}
+h2 {
+    font-family: 'Karla' sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 30px;
+    /* identical to box height, or 125% */
+    text-align: center;
+    letter-spacing: 0.01em;
+    color: #000000;
+    margin: 0.7rem 0;
+}
+p {
+    font-family: 'Karla' sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 30px;
+    /* or 167% */
+    text-align: center;
+    letter-spacing: 0.01em;
+    /* Gray */
+    color: #6E6E71;
+
+}
+</style>

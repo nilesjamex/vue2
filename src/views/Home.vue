411e4e5ab9bb398148c2567176f64a26bf9a68c8
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-13 23:35:26 +0100

    imported hero component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,13 @@
 <template>
   <Hero />
+  <Features />
 </template>
 
 <script>
 import Hero from '.././components/Hero.vue'
+import Features from '.././components/Features.vue'
 export default {
- components: { Hero }
+ components: { Hero, Features }
 }
 </script>
 

f7ae3743b6ee79e0e2ef5b04e48b596d186f398a
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-14 01:09:27 +0100

    imported new component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <Hero />
   <Features />
+  <AppFeatures />
 </template>
 
 <script>
 import Hero from '.././components/Hero.vue'
 import Features from '.././components/Features.vue'
+import AppFeatures from '.././components/AppFeatures.vue'
 export default {
- components: { Hero, Features }
+ components: { Hero, Features, AppFeatures }
 }
 </script>
 

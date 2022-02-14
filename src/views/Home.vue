26737c11e110f83412075448ba8c5ba5628bb127
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-14 23:41:12 +0100

    add inapp components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,16 @@
   <Hero />
   <Features />
   <AppFeatures />
+  <InApp />
 </template>
 
 <script>
 import Hero from '.././components/Hero.vue'
 import Features from '.././components/Features.vue'
 import AppFeatures from '.././components/AppFeatures.vue'
+import InApp from '.././components/InApp.vue'
 export default {
- components: { Hero, Features, AppFeatures }
+ components: { Hero, Features, AppFeatures, InApp }
 }
 </script>
 

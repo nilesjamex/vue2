e22a9f23e1f4a48a9e005d008166d0196390344c
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-16 17:28:26 +0100

    added new component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,8 @@
   <AppFeatures />
   <InApp />
   <InApp2 />
+  <Benefits />
+  <Waitlist />
 </template>
 
 <script>
@@ -12,8 +14,10 @@ import Features from '.././components/Features.vue'
 import AppFeatures from '.././components/AppFeatures.vue'
 import InApp from '.././components/InApp.vue'
 import InApp2 from '.././components/InApp2.vue'
+import Benefits from '.././components/Benefits.vue'
+import Waitlist from '.././components/Waitlist.vue'
 export default {
- components: { Hero, Features, AppFeatures, InApp, InApp2 }
+ components: { Hero, Features, AppFeatures, InApp, InApp2, Benefits, Waitlist }
 }
 </script>
 

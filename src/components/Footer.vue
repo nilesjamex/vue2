773decdd62e9bf44a993cd00e23aa68075d77173
egbaidomeh james <egbaidomehjamez@gmail.com>
2022-02-16 22:56:14 +0100

    new component

---
+++ src/components/Footer.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="footsplit">
+      <div>
+          <h1>
+              NIFFTY
+          </h1>
+      </div>
+      <div class="listsplit">
+          <div class="list" >
+              
+              <ul>
+              <li>
+                  <h2>
+                  About
+              </h2>
+              </li>
+              <li>About</li>
+              <li>Features</li>
+              <li>Career</li>
+              <li>Our Vision</li>
+          </ul>
+          </div>
+          
+          <div>
+              <ul>
+                  <li>
+                  <h2>
+                  Company
+              </h2>
+              </li>
+              <li>Our Mission</li>
+              <li>Our Team</li>
+              <li>Partner With Us</li>
+              <li>FAQ</li>
+          </ul>
+          </div>
+          <div>
+              <ul>
+              <li>
+                  <h2>
+                  Support
+                  </h2>
+              </li>
+              <li>Account</li>
+              <li>Support Center</li>
+              <li>Feedback</li>
+              <li>Contact Us</li>
+          </ul>
+          </div>
+         <div>
+              <ul>
+              <li>
+                  <h2>
+                  Get in touch
+                 </h2>
+              </li>
+              <li>Download App</li>
+              <li>Subscribe</li>
+          </ul>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.footsplit {
+    display: flex;
+    justify-content: space-between;
+    padding: 3rem 4rem;
+}
+h1 {
+    font-family: 'Poppins' sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 30px;
+    line-height: 45px;
+    color: #00C7BE;
+}
+.listsplit {
+    display: flex;
+    justify-content: space-between;
+    grid-gap: 2rem;
+    width: 60%;
+}
+li {
+    margin: 0 0 2rem 0;
+    font-family: 'Karla' sans-serif;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 30px;
+    letter-spacing: 0.01em;
+    color: #08102D;
+}
+h2 {
+    font-family: 'Karla' sans-serif;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 30px;
+    letter-spacing: 0.01em;
+    color: #08102D;
+}
+</style>
